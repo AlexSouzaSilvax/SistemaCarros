cbfcb3fc71999214a45695cc61de3769ce4ad9b5
Alex <alexsouza8045@gmail.com>
2018-06-19 02:39:11 -0300

    1 commit

---
+++ src/java/br/com/alex/carros/bean/UsuarioBean.java
@@ -0,0 +1,27 @@
+package br.com.alex.carros.bean;
+
+import br.com.alex.carros.dao.UsuarioDAO;
+import br.com.alex.carros.entidade.Usuario;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioBean extends CrudBean<Usuario, UsuarioDAO>{
+
+    private UsuarioDAO usuarioDAO;
+    
+    @Override
+    public UsuarioDAO getDao() {
+        if(usuarioDAO == null){
+            usuarioDAO = new UsuarioDAO();
+        }
+        return usuarioDAO;
+    }
+
+    @Override
+    public Usuario criarNovaEntidade() {
+        return new Usuario();
+    }
+    
+}
